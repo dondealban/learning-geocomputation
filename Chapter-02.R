8ab0a9c9c4bd8d7e5770ab398adc2ba89392e608
dondealban <dondealban@gmail.com>
2019-06-26 14:21:20 +0800

    Saved section on vector data

--- Chapter-02.R
+++ Chapter-02.R
@@ -1,5 +1,26 @@
 # Geocomputation with R
 # Chapter 2
 
-library(sf)
-library(raster)
+# LOAD LIBRARIES -------------------
+library(sf)          # classes and functions for vector data
+library(raster)      # classes and functions for raster data
+library(spData)      # load geographic data
+library(spDataLarge) # load larger geographic data
+
+# LOAD VIGNETTES -------------------
+vignette(package="sf") # see which vignettes are available
+vignette("sf1")          # an introduction to the package
+
+# EXPLORE VECTOR DATA --------------
+names(world)
+plot(world)
+summary(world["lifeExp"])
+world_mini <- world[1:2, 1:3]
+world_mini
+
+# Conversion of objects between sf and sp packages
+library(sp)
+world_sp <- as(world, Class="Spatial")
+world_sf <- st_as_sf(world_sp)
+
+
